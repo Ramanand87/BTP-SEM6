69ead7fef8beabde3a22a1f639ee862c52dc32d4
Ramanand87 <ram3036anand03@gmail.com>
2025-01-31 11:46:21 +0530

    cloudinary setup

--- contfarm/user/models.py
+++ contfarm/user/models.py
@@ -1,20 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 # Create your models here.
 class Profile(models.Model):
     name=models.CharField(max_length=100)
     user=models.ForeignKey(User,on_delete=models.CASCADE)
     address=models.TextField()
     phoneno=models.IntegerField()
-    image=models.ImageField(upload_to='profile/')
+    image=CloudinaryField('image')
 
     def __str__(self):
         return self.user.username
 
 class Documents(models.Model):
     doc_user=models.ForeignKey(User,on_delete=models.CASCADE)
-    doc=models.FileField(upload_to='documents/')
-
+    doc= models.FileField(upload_to='documents/')
     def __str__(self):
         return self.doc_user.username
 
+    
