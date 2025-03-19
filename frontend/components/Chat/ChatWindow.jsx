c2666bb412932097970b0bb938998776f4c55814
k29laksh <klaxay0505@gmail.com>
2025-03-19 20:37:14 +0530

    somthing new

--- frontend/components/Chat/ChatWindow.jsx
+++ frontend/components/Chat/ChatWindow.jsx
@@ -3,8 +3,8 @@ import MessageBubble from "./MessageBubble";
 
 const ChatWindow = ({ currentChat }) => {
   return (
-    <div className="flex-1 h-screen p-6 bg-blue-100">
-      <div className="max-w-2xl  h-[calc(100vh-10rem)] overflow-y-auto scrollbar-thin scrollbar-thumb-green-300 scrollbar-track-green-50 hover:scrollbar-thumb-green-400 scrollbar-thumb-rounded-full scrollbar-track-rounded-full">
+    <div className="flex-1 h-screen py-6 ">
+      <div className="px-6   h-[calc(100vh-10rem)] overflow-y-auto scrollbar-thin scrollbar-thumb-green-300 scrollbar-track-green-50 hover:scrollbar-thumb-green-400 scrollbar-thumb-rounded-full scrollbar-track-rounded-full">
         <h2 className="text-2xl font-bold mb-6">{currentChat?.name}</h2>
         <div className="space-y-4">
           {currentChat?.messages.map((message, index) => (
